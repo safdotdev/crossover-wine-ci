bedbc626f440e430f2c07073b8315ef38039ff0e
YOU54F <yousafn@gmail.com>
2023-10-02 02:32:25 +0100

    crossover 23.x

--- build_with_macports.sh
+++ build_with_macports.sh
@@ -40,8 +40,7 @@ export PACKAGE_UPLOAD=$GITHUB_WORKSPACE/upload
 export WINE_INSTALLATION=wine-cx${CROSS_OVER_VERSION}
 
 # Need to ensure port actually exists
-if ! command -v "/opt/local/bin/port" &> /dev/null
-then
+if ! command -v "/opt/local/bin/port" &>/dev/null; then
     echo "</opt/local/bin/port> could not be found"
     echo "A macports installation is required"
     exit
@@ -50,16 +49,13 @@ fi
 # Manually configure $PATH
 export PATH="/opt/local/libexec/llvm-cx/bin:/opt/opt/bin:/opt/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin"
 
-
 begingroup "Installing dependencies build"
 sudo port install bison ccache gettext llvm-cx mingw-w64 pkgconfig
 endgroup
 
-
 begingroup "Installing dependencies libraries"
 sudo port install freetype gettext-runtime gnutls moltenvk libpcap libsdl2
 endgroup
-
 
 export CC="ccache clang"
 export CXX="${CC}++"
@@ -73,13 +69,11 @@ export LDFLAGS="-Wl,-headerpad_max_install_names -Wl,-rpath,@loader_path/../../ 
 export ac_cv_lib_soname_MoltenVK="libMoltenVK.dylib"
 export ac_cv_lib_soname_vulkan=""
 
-
 if [[ ! -f ${CROSS_OVER_LOCAL_FILE}.tar.gz ]]; then
     begingroup "Downloading $CROSS_OVER_LOCAL_FILE"
     curl -o ${CROSS_OVER_LOCAL_FILE}.tar.gz ${CROSS_OVER_SOURCE_URL}
     endgroup
 fi
-
 
 begingroup "Extracting $CROSS_OVER_LOCAL_FILE"
 if [[ -d "${GITHUB_WORKSPACE}/sources" ]]; then
@@ -88,11 +82,10 @@ fi
 tar xf ${CROSS_OVER_LOCAL_FILE}.tar.gz
 endgroup
 
-
 +begingroup "Add distversion.h"
 cp ${GITHUB_WORKSPACE}/distversion.h ${GITHUB_WORKSPACE}/sources/wine/include/distversion.h
+patch -p1 <${GITHUB_WORKSPACE}/distversion23.patch
 endgroup
-
 
 begingroup "Configure wine64-${CROSS_OVER_VERSION}"
 mkdir -p ${BUILDROOT}/wine64-${CROSS_OVER_VERSION}
@@ -138,88 +131,82 @@ ${WINE_CONFIGURE} \
 popd
 endgroup
 
-
 begingroup "Build wine64-${CROSS_OVER_VERSION}"
 pushd ${BUILDROOT}/wine64-${CROSS_OVER_VERSION}
 make -j$(sysctl -n hw.ncpu 2>/dev/null)
 popd
 endgroup
 
+if [[ -z ${SKIP_WINE32} ]]; then
+    begingroup "Configure wine32on64-${CROSS_OVER_VERSION}"
+    mkdir -p ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
+    pushd ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
+    ${WINE_CONFIGURE} \
+        CROSSCC="ccache i686-w64-mingw32-gcc" \
+        --prefix= \
+        --disable-loader \
+        --disable-tests \
+        --disable-winedbg \
+        --enable-win32on64 \
+        --without-alsa \
+        --without-capi \
+        --with-coreaudio \
+        --with-cups \
+        --without-dbus \
+        --without-fontconfig \
+        --with-freetype \
+        --with-gettext \
+        --without-gettextpo \
+        --without-gphoto \
+        --with-gnutls \
+        --without-gssapi \
+        --without-gstreamer \
+        --without-inotify \
+        --without-krb5 \
+        --with-mingw \
+        --without-netapi \
+        --without-openal \
+        --with-opencl \
+        --with-opengl \
+        --without-oss \
+        --with-pcap \
+        --with-pthread \
+        --without-pulse \
+        --without-sane \
+        --with-sdl \
+        --without-udev \
+        --with-unwind \
+        --without-usb \
+        --without-v4l2 \
+        --with-vulkan \
+        --with-wine64=${BUILDROOT}/wine64-${CROSS_OVER_VERSION} \
+        --without-x
+    popd
+    endgroup
 
-begingroup "Configure wine32on64-${CROSS_OVER_VERSION}"
-mkdir -p ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
-pushd ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
-${WINE_CONFIGURE} \
-    CROSSCC="ccache i686-w64-mingw32-gcc" \
-    --prefix= \
-    --disable-loader \
-    --disable-tests \
-    --disable-winedbg \
-    --enable-win32on64 \
-    --without-alsa \
-    --without-capi \
-    --with-coreaudio \
-    --with-cups \
-    --without-dbus \
-    --without-fontconfig \
-    --with-freetype \
-    --with-gettext \
-    --without-gettextpo \
-    --without-gphoto \
-    --with-gnutls \
-    --without-gssapi \
-    --without-gstreamer \
-    --without-inotify \
-    --without-krb5 \
-    --with-mingw \
-    --without-netapi \
-    --without-openal \
-    --with-opencl \
-    --with-opengl \
-    --without-oss \
-    --with-pcap \
-    --with-pthread \
-    --without-pulse \
-    --without-sane \
-    --with-sdl \
-    --without-udev \
-    --with-unwind \
-    --without-usb \
-    --without-v4l2 \
-    --with-vulkan \
-    --with-wine64=${BUILDROOT}/wine64-${CROSS_OVER_VERSION} \
-    --without-x
-popd
-endgroup
+    begingroup "Build wine32on64-${CROSS_OVER_VERSION}"
+    pushd ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
+    make -j$(sysctl -n hw.activecpu 2>/dev/null)
+    popd
+    endgroup
 
-
-begingroup "Build wine32on64-${CROSS_OVER_VERSION}"
-pushd ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
-make -j$(sysctl -n hw.activecpu 2>/dev/null)
-popd
-endgroup
-
-
-begingroup "Install wine32on64-${CROSS_OVER_VERSION}"
-pushd ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
-make install-lib DESTDIR="${INSTALLROOT}/${WINE_INSTALLATION}"
-popd
-endgroup
-
-
+    begingroup "Install wine32on64-${CROSS_OVER_VERSION}"
+    pushd ${BUILDROOT}/wine32on64-${CROSS_OVER_VERSION}
+    make install-lib DESTDIR="${INSTALLROOT}/${WINE_INSTALLATION}"
+    popd
+    endgroup
+fi
 begingroup "Install wine64-${CROSS_OVER_VERSION}"
 pushd ${BUILDROOT}/wine64-${CROSS_OVER_VERSION}
 make install-lib DESTDIR="${INSTALLROOT}/${WINE_INSTALLATION}"
 popd
 endgroup
 
-
 begingroup "Tar Wine"
 pushd ${INSTALLROOT}
 tar -czvf ${WINE_INSTALLATION}.tar.gz ${WINE_INSTALLATION}
 popd
 endgroup
-
 
 begingroup "Upload Wine"
 mkdir -p ${PACKAGE_UPLOAD}
